ddcbbedce1663bb3420920469bb35d3df5b09b64
Tuan Vu <vuanhtuan112ll@gmail.com>
2026-02-02 02:28:02 +0700

    create project

---
+++ LTW/src/main/java/_2/LTW/LtwApplication.java
@@ -0,0 +1,13 @@
+package _2.LTW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtwApplication.class, args);
+	}
+
+}
---
+++ LTW/src/test/java/_2/LTW/LtwApplicationTests.java
@@ -0,0 +1,13 @@
+package _2.LTW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
